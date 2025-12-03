6352bcf031d86307e26963867919cd5573976a47
Eloy Dominguez <eloydominguez3@gmail.com>
2025-12-03 01:26:18 +0100

    Lectura de la API

--- read_API.py
+++ read_API.py
@@ -3,36 +3,43 @@ import requests
 import plotext as pl
 
 def grafica_inflacion():
-    response = requests.get("https://servicios.ine.es/wstempus/js/ES/DATOS_TABLA/24077")
+    response = requests.get("https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/IPC251856?nult=13")
     data = response.json()
    
-    # Creamos el dataframe
     df = pd.DataFrame(data)
-    # Normalizamos el json dentro del dataframe
-    df_exp = pd.json_normalize(df['Data'][0])
-    # Eliminamos columnas innecessarias
-    df_exp.drop(columns=["Secreto", "FK_TipoDato","Fecha"], inplace=True)
-    # Creamos una columna nueva que se guardara como string el año y el mes
-    df_exp['Periodo'] = df_exp['Anyo'].astype(str) + "-" + df_exp['FK_Periodo'].astype(str).str.zfill(2)
-    # Ordenamos por año y mes
-    df_exp = df_exp.sort_values(['Anyo','FK_Periodo'])
-    # Calculamos en una nueva columna la inflacio interanual
-    df_exp['Inflacion_interanual'] = df_exp['Valor'].pct_change(12) * 100
-    # Creamos un nuevo dataframe con los datos que nos interesan
-    df_nuevo = pd.DataFrame(df_exp[['Periodo','Inflacion_interanual']])
-    # Por ultimo hacemos que solo sean dos decimales y ordenamos el indice
-    df_nuevo["Inflacion_interanual"] = df_nuevo["Inflacion_interanual"].round(2)
-    df_nuevo.sort_index(inplace=True)
+    df_nuevo = pd.json_normalize(df["Data"])
 
-    inflacion = df_nuevo.head(n=13)
+    df_nuevo.drop(columns=["Fecha", "FK_TipoDato", "Secreto"], inplace=True)
 
-    promedio = inflacion["Inflacion_interanual"].mean()
+    df_nuevo ["FK_Periodo"] = df_nuevo["FK_Periodo"].astype(str)  
+    df_nuevo["Anyo"] = df_nuevo["Anyo"].astype(str)
 
-    x = inflacion["Periodo"].tolist()
-    y = inflacion["Inflacion_interanual"].tolist()
+    df_nuevo["Periodo"] = df_nuevo["FK_Periodo"] + "/" + df_nuevo["Anyo"]
+    
+    df_nuevo.drop(columns=["FK_Periodo", "Anyo"], inplace=True)
+    
 
-    pl.plot(range(len(x)),y)
-    pl.title("Gráfico de Inflacion Anual")
-    pl.ylabel("Inflacion")
-    pl.xlabel("Mes-Año")
+    lista_periodo = df_nuevo["Periodo"].to_list()
+    x = range(len(lista_periodo))
+    y = df_nuevo["Valor"].to_list()
+
+    valor_maximo = df_nuevo["Valor"].max()
+
+    valor_minimo = df_nuevo["Valor"].min()
+
+    pl.plot(x,y,  marker="dot", color="red")
+    pl.title("Inflacion Interanual")
+    pl.yscale("linear")
+    pl.ylim(valor_minimo,valor_maximo)
+    pl.xticks(x, lista_periodo)
+    pl.canvas_color("black")
+    pl.axes_color("black")
+    pl.plotsize(60,35)
     pl.show()
+
+    
+
+    
+
+
+    
